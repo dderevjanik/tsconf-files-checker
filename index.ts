8dfa63d5e6d3dbac4ec5cef7b5cb01d3b2fd610d
dderevjanik <daniel.derevjanik@gmail.com>
2019-08-17 16:08:53 +0200

    Updated cli help, added verbose, more TODOs

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+// TODO: Convert to jsfile with @ts-check enabled. This approach would be better for cli programs
 // TODO: Add more meaningful errors with explanation
 import * as ts from "typescript";
 import nconf from "nconf";
@@ -11,9 +12,9 @@ type Config = (typeof config) & {
     _: string[];
     h?: boolean;
     help?: boolean;
-    project?: string; // Path to tsconfig
+    project?: string; // Path to project's tsconfig
+    verbose?: boolean;
     // TODO: Add --update, which automatically update tsconfig with successFiles
-    // TODO: Add --verbose
 };
 
 const conf = nconf
@@ -24,13 +25,25 @@ const conf = nconf
 if (conf.h || conf.help || conf._.length === 0) {
     process.stdout.write("Usage: ts-node --project TSCONFIG_PATH SRC_PATH\n");
     process.stdout.write("\n");
-    // TODO: Add example
-    process.exit();
+    process.stdout.write("Example: ts-node ./index.ts --project ../App/tsconfig.strict.json ../App/src\n");
+    process.stdout.write("\n");
+    process.stdout.write("\t--project\tpath to your tsconfig.json\n");
+    process.stdout.write("\t--verbose\tprint all logs, usefull for debugging\n");
+    // TODO: Add --update
+    process.exit(0);
 }
 
-function check(inputFileNames: string[], options: ts.CompilerOptions): void {
-    const absolutePaths = inputFileNames.map(f => path.resolve(f));
-    const program = ts.createProgram(absolutePaths, options);
+let verbose: boolean;
+
+function check(inputFileNames: string[], options: ts.CompilerOptions) {
+    // Obey 'node_modules/' files
+    const nodeModulesRE = new RegExp(/node_modules/);
+    const absolutePathsWithoutNodeModules = inputFileNames
+        .filter(f => !nodeModulesRE.test(f))
+        .map(f => path.resolve(f));
+    console.log(`skip ${inputFileNames.length - absolutePathsWithoutNodeModules.length} node_modules files`);
+    console.log(`runnig program against ${absolutePathsWithoutNodeModules.length} .ts files`);
+    const program = ts.createProgram(absolutePathsWithoutNodeModules, options);
 
     const errorFiles = new Set<string>();
     const diagnostics = program.getSemanticDiagnostics();
@@ -43,24 +56,36 @@ function check(inputFileNames: string[], options: ts.CompilerOptions): void {
             }
         }
     });
-    const successFiles = inputFileNames.filter((filePath) => !errorFiles.has(filePath));
+    const successFiles = absolutePathsWithoutNodeModules.filter((filePath) => !errorFiles.has(filePath));
 
-    console.log('success', successFiles);
-    console.log('errors', errorFiles);
+    // TODO: Print files path relative to passed tsconfig dirname
+    return {
+        successFiles,
+        errorFiles
+    };
 }
 
 (async function() {
     let tsconf: any;
+    let tsconfDirName: string;
 
-    // TODO: If not defined, try to find nearest tsconfig.json in CWD using ts.findConf
+    if (conf.verbose) {
+        verbose = true;
+    }
+
+    // TODO: If not defined, try to find nearest tsconfig.json in CWD using ts.findConf. Inform user about that
     if (conf.project !== undefined) {
+        console.log('checking existence of tsconfig');
         if (fs.existsSync(conf.project)) {
             let file;
             try {
+                console.log('reading tsconfig');
                 file = fs.readFileSync(conf.project);
+                tsconfDirName = path.dirname(conf.project);
             } catch(err) {
                 throw new Error('CANNOT_READ_TSCONFIG');
             }
+            console.log('parsing tsconfig');
             tsconf = ts.parseConfigFileTextToJson('test', file.toString());
             // NOTE: Because tsconfig.json can have comments, JSON.parse() will throw errors
             if (tsconf.error) {
@@ -76,11 +101,17 @@ function check(inputFileNames: string[], options: ts.CompilerOptions): void {
     if (conf._.length === 0) {
         throw new Error('MISSING_SRC_PATH');
     } else {
+        console.log('applying glob patter to find all .ts files in project');
         glob(path.resolve(conf._[0], '**' ,'*.ts'), (err, files) => {
             if (err) {
                 throw new Error('GLOB_ERROR');
             }
-            check(files, tsconf.config);
+            console.log(`found ${files.length} files in project`);
+            const { successFiles, errorFiles } = check(files, tsconf.config);
+            console.log(`successFiles count: ${successFiles.length}`);
+            console.log(`errorFiles count: ${errorFiles.size}`);
+            // console.log(successFiles.map(p => path.relative(tsconfDirName, p)));
+            // console.log(errorFiles.map(p => path.relative(tsconfDirName, p)));
             process.exit(0);
         });
     }
